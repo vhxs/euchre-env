59da7813b053c5c279e1e861d6c17772de3be0c2
vsaraph <vikram.saraph.22@gmail.com>
2022-08-28 17:48:24 -0400

    I think the game is functional now? Need to clean up prints though. And restructure code

--- euchre.py
+++ euchre.py
@@ -1,103 +1,65 @@
 import random
 
-class Suits:
-    HEART, SPADE, DIAMOND, CLUB = range(4)
-    suits_repr = {HEART: 'H', SPADE: 'S', DIAMOND: 'D', CLUB: 'C'}
-    values_repr = ['9', '10', 'J', 'Q', 'K', 'A']
-    suits_ids = {y: x for x, y in suits_repr.items()}
-    values_ids = {val: i for i, val in enumerate(values_repr)}
-
-    suits_complement = {HEART: DIAMOND, DIAMOND: HEART, SPADE: CLUB, CLUB: SPADE}
-
-    def order(lead_suit, trump_suit):
-        # there are 24 cards in a euchre deck
-        radix = 24
-        coef = 1
-        scores = {}
-
-        # not trump, not lead suit
-        for suit in [s for s in range(4) if s not in [lead_suit, trump_suit]]:
-            for value in range(6):
-                scores[Card(suit, value)] = 0
-
-        # leading suit
-        coef *= radix
-        for value in range(6):
-            scores[Card[lead_suit, value]] = coef + value
-
-        # trump suit
-        coef *= radix
-        for value in range(6):
-            scores[Card[trump_suit, value]] = coef + value
-
-        # left bower
-        
-
-    def choose_winner(plays):
-        pass
 
 class Card:
+
     def __init__(self, suit, value):
-        # "real" suit is only for printing/parsing
-        self.real_suit = suit
+        self.face_suit = suit
         self.suit = suit
         self.value = value
 
     def __str__(self):
-        suits_repr = Suits.suits_repr[self.real_suit]
-        values_repr = Suits.values_repr[self.value]
+        suits_repr = Deck.suits_repr[self.face_suit]
+        values_repr = Deck.values_repr[self.value]
         return f"{values_repr}{suits_repr}"
 
     def __eq__(self, other):
-        return (self.suit == other.suit) and (self.value == other.value)
+        return (self.face_suit == other.face_suit) and (self.value == other.value)
 
-    def make_trump(self, trump_suit):
-        # should only be called on a Jack
-        if self.value != Suits.values_ids['J']:
-            raise ValueError("Trying to make non-Jack a trump card!")
-        
-        self.suit = trump_suit
 
 class Player:
-    def __init__(self, idx):
+    def __init__(self, idx, deck):
         self.idx = idx
+        self.deck = deck
         self.hand = []
 
     def deal(self, cards):
         self.hand.extend(cards)
 
     def remove_card(self, card):
-        self.cards.remove(card)
+        self.hand.remove(card)
 
     def print_hand(self):
         print("Hand: " + " ".join([str(card) for card in self.hand]))
 
     def choose_card(self, prompt, lead_suit=None):
         while True:
+            print(f"Player {self.idx}")
             self.print_hand()
             card_str = input(prompt)
 
             # parse input
             try:
-                suit = Suits.suits_ids[card_str[-1]]
-                value = Suits.values_ids[card_str[:-1]]
+                suit = Deck.suits_ids[card_str[-1]]
+                value = Deck.values_ids[card_str[:-1]]
+            # garbage input
             except (IndexError, KeyError):
-                print(f"Invalid card {card_str}!")
+                print(f"{card_str} is not a card!")
                 continue
 
             # valid card, but not in hand
-            card = Card(suit, value)
+            card = self.deck.get_card(suit, value)
             if card not in self.hand:
                 print(f"Card {card_str} not in hand!")
                 continue
 
             # doesn't match lead suit
-            if lead_suit is not None and self.has_suit(lead_suit):
+            if lead_suit is not None and card.suit != lead_suit and self.has_suit(lead_suit):
                 print(f"Must follow suit!")
                 continue
 
-            break
-        return card
+            print()
+            return card
 
     def swap_card(self, new_card):
         # first pick up new card
@@ -119,32 +81,85 @@ class Player:
         card = self.choose_card("Which card to play? ", lead_suit)
         self.hand.remove(card)
         return card
-    
+
+
 class Deck:
+    HEART, SPADE, DIAMOND, CLUB = SUITS = range(4)
+    NINE, TEN, JACK, QUEEN, KING, ACE = VALUES = range(6)
+
+    suits_repr = {HEART: 'H', SPADE: 'S', DIAMOND: 'D', CLUB: 'C'}
+    values_repr = ['9', '10', 'J', 'Q', 'K', 'A']
+    suits_ids = {y: x for x, y in suits_repr.items()}
+    values_ids = {val: i for i, val in enumerate(values_repr)}
+
+    suits_complement = {HEART: DIAMOND, DIAMOND: HEART, SPADE: CLUB, CLUB: SPADE}
+
     def __init__(self):
-        self.cards = []
+        # create card objects, shouldn't change over lifetime of program
+        self.cards = {}
+        for suit in Deck.SUITS:
+            for value in Deck.VALUES:
+                card = Card(suit, value)
+                self.cards[(suit, value)] = card
+
+        # populate deck
         self.reset()
 
     def reset(self):
-        # populate deck
-        self.cards.clear()
-        for suit in range(4):
-            for value in range(6):
-                self.cards.append(Card(suit, value))
-        # shuffle it
-        random.shuffle(self.cards)
+        for card in self.cards.values():
+            card.suit = card.face_suit
+        self.deck = list(self.cards.values())
+        random.shuffle(self.deck)
 
     def deal(self, player):
         to_deal = []
         for _ in range(5):
-            to_deal.append(self.cards.pop())
+            card = self.take_top()
+            to_deal.append(card)
         player.deal(to_deal)
 
+    def get_card(self, face_suit, value):
+        return self.cards[(face_suit, value)]
+
     def take_top(self):
-        return self.cards.pop()
+        return self.deck.pop()
 
     def add(self, card):
-        self.cards.append(card)
+        self.deck.append(card)
+
+    def score_cards(self, lead_suit, trump_suit):
+        self.score = {}
+        for card in self.cards.values():
+            if card.suit == lead_suit:
+                score = 24 + card.value
+            elif card.suit == trump_suit:
+                # a bower
+                if card.value == Deck.JACK:
+                    # the right
+                    if card.face_suit == trump_suit:
+                        score = 24 * 24 * 24 * 24
+                    # the left
+                    else:
+                        score = 24 * 24 * 24
+                # not a bower
+                else:
+                    score = 24 * 24 + card.value
+            # just a card
+            else:
+                score = -1
+            self.score[(card.face_suit, card.value)] = score
+
+    def get_score(self, card):
+        face_suit = card.face_suit
+        value = card.value
+
+        return self.score[(face_suit, value)]
+
+    def make_trump(self, trump):
+        opposite_suit = Deck.suits_complement[trump]
+        left = self.get_card(opposite_suit, Deck.JACK)
+        left.suit = trump
+
 
 class Euchre:
     def __init__(self, stick_the_dealer=False):
@@ -153,9 +168,8 @@ class Euchre:
         self.deck = Deck()
         self.dealer = random.randint(0, 3)
         self.stick_the_dealer = stick_the_dealer
-        self.players = [Player(p) for p in range(4)]
+        self.players = [Player(p, self.deck) for p in range(4)]
 
-        # exit gracefully
         try:
             self.play_game()
         except KeyboardInterrupt:
@@ -178,6 +192,10 @@ class Euchre:
         for player in self.players:
             player.hand.clear()
 
+    def throw_in(self):
+        self.reset()
+        self.deck.reset()
+
     def play_round(self):
         # deal to everyone
         self.reset()
@@ -185,22 +203,22 @@ class Euchre:
         for player in self.players:
             self.deck.deal(player)
 
-        # pick a suit
-        success, caller, alone, suit = self.bidding_round_one()
+        # pick a suit and caller
+        success, caller, alone, trump = self.bidding_round_one()
         if not success:
-            success, caller, alone, suit = self.bidding_round_two(suit)
+            success, caller, alone, trump = self.bidding_round_two(trump)
             if not success:
                 # should not happen in stick the dealer
                 assert not self.stick_the_dealer
                 print("Throw it in!")
                 self.throw_in()
                 self.play_round()
-        else:
-            trump = suit
-        
-        # play 5 tricks
-        tricks1 = 0
-        tricks2 = 0
+
+        # change suit of the left
+        self.deck.make_trump(trump)
+
+        # track only caller's tricks
+        tricks = 0
         leader = (self.dealer + 1) % 4
 
         # skip partner if alone
@@ -211,33 +229,34 @@ class Euchre:
         else:
             skip_player = None
 
+        # play 5 tricks, keep track of caller's tricks
         for _ in range(5):
             leader = self.play_trick(leader, trump, skip_player)
-            if leader % 2 == 0:
-                tricks1 += 1
-            else:
-                tricks2 += 1
-        
-        # determine scoring, based on caller, alone, outcome
-        # TODO calculate score
+            if leader % 2 == caller % 2:
+                tricks += 1
 
-        return 0, 10
-
-    def compute_winner_score(self, did_call, tricks, alone):
-        if did_call:
-            if tricks == 5:
-                if not alone:
-                    # called it and got all tricks, with partner
-                    return 2
-                else:
-                    # got all of them alone
-                    return 4
-            else:
-                # didn't get them all, alone or otherwise
-                return 1
+        # not a euchre
+        if tricks >= 3:
+            winner = caller % 2
+            score = self.compute_winner_score(tricks, alone)
+        # euchred
         else:
-            # this is a euchre
-            return 2
+            winner = (caller + 1) % 2
+            score = 2
+
+        return winner, score
+
+    def compute_winner_score(self, tricks, alone):
+        if tricks == 5:
+            if not alone:
+                # called it and got all tricks, with partner
+                return 2
+            else:
+                # got all of them alone
+                return 4
+        else:
+            # didn't get them all, alone or otherwise
+            return 1
 
     def play_trick(self, leader, trump, skip_player=None):
         suit = None
@@ -251,17 +270,22 @@ class Euchre:
                 continue
 
             # choose card to play, following suit
-            card = self.players[player].choose_card("What to play? ", suit)
+            card = self.players[player].play_card(suit)
 
             # set suit and card order if first player
             if suit is None:
                 suit = card.suit
-                Suits.order(suit, trump)
+                self.deck.score_cards(suit, trump)
 
             # add to the trick
             plays[player] = card
 
-        return 0
+        sorted_plays = sorted([(self.deck.get_score(card), player) for player, card in plays.items()], reverse=True)
+        new_leader = sorted_plays[0][1]
+
+        print("Trick over")
+
+        return new_leader
 
     def bidding_round_one(self):
         maybe_trump_card = self.deck.take_top()
@@ -278,10 +302,10 @@ class Euchre:
         return False, -1, False, maybe_trump_card.suit
 
     def choose_suit(self, player, forbidden_suit, force_choice):
-        possible_suits = [suit for suit in player.suits_in_hand() if suit != forbidden_suit]
+        possible_suits = set([suit for suit in player.suits_in_hand() if suit != forbidden_suit])
         # check edge case
         if not possible_suits and force_choice:
-            print(f"Forced to choose {Suits.suits_repr[forbidden_suit]}.")
+            print(f"Forced to choose {Deck.suits_repr[forbidden_suit]}.")
             alone_choice = input("Alone? (y/n): ")
             if alone_choice.lower() == 'y':
                 alone = True
@@ -289,7 +313,7 @@ class Euchre:
                 alone = False
             return True, forbidden_suit, alone
 
-        possible_suits_str = " ".join([Suits.suits_repr[suit] for suit in possible_suits])
+        possible_suits_str = " ".join([Deck.suits_repr[suit] for suit in possible_suits])
         if not force_choice:
             prompt = f"Choose suit among {possible_suits_str} or pass (p): "
         else:
@@ -301,7 +325,7 @@ class Euchre:
                 return False, -1, False
             # otherwise choose trump
             try:
-                choice = Suits.suits_ids[choice_str]
+                choice = Deck.suits_ids[choice_str]
             except (IndexError, KeyError):
                 print(f"Invalid choice {choice_str}")
             if choice not in possible_suits:
@@ -327,7 +351,7 @@ class Euchre:
             player_number = (player_number + 1) % 4
         # this can't happen in stick the dealer
         return False, -1, False, -1
-        
+
     def finished(self):
         if self.score1 >= 10 or self.score2 >= 10:
             return True
@@ -338,6 +362,7 @@ class Euchre:
             print("Team 1 wins!")
         else:
             print("Team 2 wins!")
+
 
 if __name__ == "__main__":
     game = Euchre()
